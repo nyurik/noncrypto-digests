c415df60e3b872ffff6dbdf23edb8c844d36208d
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2023-12-21 00:32:53 -0500

    linting, msrv

--- src/lib.rs
+++ src/lib.rs
@@ -14,22 +14,22 @@ mod common;
 #[cfg(feature = "fnv")]
 mod fnv;
 #[cfg(feature = "fnv")]
-pub use fnv::{Fnv, FnvHasher};
+pub use crate::fnv::{Fnv, FnvHasher};
 
 #[cfg(feature = "xxh3")]
 mod xxh3;
 #[cfg(feature = "xxh3")]
-pub use xxh3::{Xxh3Hasher, Xxh3_128, Xxh3_64};
+pub use crate::xxh3::{Xxh3Hasher, Xxh3_128, Xxh3_64};
 
 #[cfg(feature = "xxh32")]
 mod xxh32;
 #[cfg(feature = "xxh32")]
-pub use xxh32::{Xxh32, Xxh32Hasher};
+pub use crate::xxh32::{Xxh32, Xxh32Hasher};
 
 #[cfg(feature = "xxh64")]
 mod xxh64;
 #[cfg(feature = "xxh64")]
-pub use xxh64::{Xxh64, Xxh64Hasher};
+pub use crate::xxh64::{Xxh64, Xxh64Hasher};
 
 #[cfg(test)]
 mod tests {
